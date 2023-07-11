0db71cd98b53c0e320f6e02fdb5a696358cff97a
alienchio <100249979+alienchio@users.noreply.github.com>
2023-07-11 18:16:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//Funcion para cambiar color de fondo
+const changeColorButton = document.getElementById("changeColorButton");
+const videoElement = document.getElementById("videoElement");
+
+changeColorButton.addEventListener("click", function() {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+//Funcion con fetch
 document.addEventListener('DOMContentLoaded', function() {
   var userTableBody = document.querySelector('#userTable tbody');
 
